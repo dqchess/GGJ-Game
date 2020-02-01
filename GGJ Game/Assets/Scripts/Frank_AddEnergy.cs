401b5727c7f71772e6791a697954e5e551f3cace
frank078 <46834129+frank078@users.noreply.github.com>
2020-01-31 22:29:52 -0500

    EnergyUI Functionality
    Added functionality to the add energy UI

---
+++ GGJ Game/Assets/Scripts/Frank_AddEnergy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Frank_AddEnergy : MonoBehaviour
+{
+    public Image engineOff;
+    public Image engineOn;
+    public Image energyFiller;
+
+    public void ToggleEngine()
+    {
+        engineOff.gameObject.SetActive(!engineOff.isActiveAndEnabled);
+        engineOn.gameObject.SetActive(!engineOn.isActiveAndEnabled);
+    }
+
+    public void AddEnergy()
+    {
+        energyFiller.fillAmount -= .2f;
+    }
+}
