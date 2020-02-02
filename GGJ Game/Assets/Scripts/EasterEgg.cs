33b2377bdde3aa041d97d3d063e8436d95654608
EveAvacyn <eveavacyn@gmail.com>
2020-02-02 14:17:26 -0500

    new

--- GGJ Game/Assets/Scripts/EasterEgg.cs
+++ GGJ Game/Assets/Scripts/EasterEgg.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EasterEgg : MonoBehaviour
 {
+    public int state;
+    public GameObject ship1;
+    public GameObject ship2;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,37 @@ public class EasterEgg : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.N))
+            state = 1;
+        if (Input.GetKeyDown(KeyCode.C))
+            if (state == 1)
+                state = 2;
+            else if (state == 2)
+                state = 3;
+            else
+                state = 0;
+        if (Input.GetKeyDown(KeyCode.Alpha1)|| Input.GetKeyDown(KeyCode.Keypad1))
+            if (state == 3)
+                state = 4;
+            else if (state == 6)
+                state = 7;
+        if (Input.GetKeyDown(KeyCode.Alpha7) || Input.GetKeyDown(KeyCode.Keypad7))
+            if (state == 4)
+                state = 5;
+            else
+                state = 0;
+        if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
+            if (state == 5)
+                state = 6;
+            else
+                state = 0;
+
+        if (state == 7)
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                ship1.SetActive(false);
+                ship2.SetActive(true);
+            }
+
     }
 }
