b05f8923ec01ce6fda189ef8ab4f2f6a0c02f3b8
Dicky Chouryan <dchouryan26@gmail.com>
2020-02-02 03:40:45 -0500

    main menu, play and quit

---
+++ GGJ Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void Tutorial()
+    {
+        //open new panel
+    }
+
+    public void Credits()
+    {
+        //open new panel
+    }
+}
