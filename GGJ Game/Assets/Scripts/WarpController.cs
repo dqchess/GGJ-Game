8e99f6b104a991bbc8e3687961b04744867d0a48
EveAvacyn <eveavacyn@gmail.com>
2020-02-02 08:45:52 -0500

    warpspeed

---
+++ GGJ Game/Assets/Scripts/WarpController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpController : MonoBehaviour
+{
+    public float startZ;
+    public float startScale;
+    public float endScale;
+    public float cScale;
+    public float time;
+    public GameObject warpBubble;
+    private float moveSpeed;
+    private bool warp = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = startZ / time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void gotothewarp()
+    {
+        warp = true;
+    }
+    private void warpspeed()
+    {
+        if (warp)
+        {
+            warpBubble.SetActive(true);
+            if (transform.position.z > 0)
+            {
+
+                transform.position -= Vector3.forward * Time.deltaTime * moveSpeed;
+                cScale = (1 - transform.position.z / startZ) * endScale;
+                transform.localScale = new Vector3(cScale, cScale, cScale);
+
+            }
+        }
+    }
+}
---
+++ GGJ Game/Assets/Warp_Effect/Assets/Scripts/WarpEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace com.ggames4u {
+
+    /// <summary>
+    /// This script has an example of how to get access to the warp shpere from the root game object.
+    /// </summary>
+    [HelpURL("http://blog.ggames4u.com/wp-content/uploads/2019/06/WarpEffect_Manual_Version-1.6.1.pdf")]
+    public class WarpEffect : MonoBehaviour {
+        [SerializeField]
+        private Material warpMaterial;
+
+        /// <summary>
+        /// This script has an example of how to get access to the warp shpere from the root game object.
+        /// </summary>
+        private void Awake() {
+            GameObject warpSphere = this.transform.Find("WarpSphereContainer").Find("WarpSphere").gameObject;
+            this.warpMaterial = warpSphere.GetComponent<Renderer>().sharedMaterial;
+            if (this.warpMaterial != null) {
+                // Do somthing
+            }
+        }
+    }
+}
