31f37610f4d61ec2dbed35045dae6a1be560eb96
frank078 <46834129+frank078@users.noreply.github.com>
2020-02-02 11:35:04 -0500

    explostions

---
+++ GGJ Game/Assets/Scripts/ExplotionStop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplotionStop : MonoBehaviour
+{
+    float timer = 2.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
