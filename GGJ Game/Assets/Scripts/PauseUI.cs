3ddb3f0a284b67fcd1e4e10465f76e247dace18f
Dicky Chouryan <dchouryan26@gmail.com>
2020-02-02 01:47:06 -0500

    Pause

--- GGJ Game/Assets/Scripts/PauseUI.cs
+++ GGJ Game/Assets/Scripts/PauseUI.cs
@@ -8,22 +8,64 @@ public class PauseUI : MonoBehaviour
     public ShipStatus shipStatus;
 
     public GameObject youLose;
+    public GameObject pause;
 
+    public bool isPaused = false;
+
+    private void Start()
+    {
+        if (isPaused)
+        {
+            isPaused = false;
+        }
+       pause.SetActive(false);
+    }
     private void Update()
     {
         if(shipStatus.health <= 0)
         {
             youLose.SetActive(true);
+            pause.SetActive(false); // SAFETY
         }
         else
         {
             youLose.SetActive(false);
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (shipStatus.health > 0) // If player died then player can't press pause
+            {
+                if(isPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+
+        }
     }
 
+    public void Resume()
+    {
+        pause.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pause.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
     }
 
     public void MainMenu()
